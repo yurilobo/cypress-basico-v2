625c9608356d6777322299f431fe10e62f4436e6
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2022-08-19 16:12:52 -0300

    Acessando o link de destino

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -111,7 +111,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique',function(){
         cy.get('#privacy a').should('have.attr','target','_blank')
     })
-    it.only('acessa a página da política de privacidade removendo o target e então clicanco no link',function(){
+    it('acessa a página da política de privacidade removendo o target e então clicanco no link',function(){
         cy.get('#privacy a')
             .invoke('removeAttr','target')
             .click()
