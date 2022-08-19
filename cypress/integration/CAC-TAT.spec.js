1953777515eddba91fd55b1afe334b4312f1768a
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2022-08-19 16:01:41 -0300

    Removendo o Target

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -108,7 +108,13 @@ describe('Central de Atendimento ao Cliente TAT', function() {
                 expect($input[0].files[0].name).to.be.equal('example.json')
             })
     })
-    it.only('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique',function(){
+    it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique',function(){
         cy.get('#privacy a').should('have.attr','target','_blank')
+    })
+    it.only('acessa a página da política de privacidade removendo o target e então clicanco no link',function(){
+        cy.get('#privacy a')
+            .invoke('removeAttr','target')
+            .click()
+        cy.contains('Talking About Testing').should('be.visible')
     })
   })
