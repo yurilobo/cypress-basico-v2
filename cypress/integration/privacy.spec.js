8e44b52b0d3a184e85c98b7436118bb45fb7d4a9
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2022-08-20 01:22:48 -0300

    Usando o Lodash para fazer o teste 10 vezes

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,4 +1,7 @@
-it( 'testa a página da política de privavidade de forma independente'  ,function(){
-    cy.visit('./src/privacy.html')
-    cy.contains('Talking About Testing').should('be.visible')
+
+Cypress._.times(20,function(){
+    it( 'testa a página da política de privavidade de forma independente'  ,function(){
+        cy.visit('./src/privacy.html')
+        cy.contains('Talking About Testing').should('be.visible')
+    })
 })
